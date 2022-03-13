96d04a9b7e436f0a451d887bbec0bb5937838cc6
Zié Amara <90223980+ZieAmara@users.noreply.github.com>
2022-03-13 20:07:37 +0000

    Create Propriete.java

---
+++ Propriete.java
@@ -0,0 +1,11 @@
+package packs;
+
+class Propriete{
+  String titre;
+  String operateur;
+  
+  Public Propriete(String name, String operateur = ""){
+    this.titre = name;
+    this.operateur = operateur;
+  }
+}
