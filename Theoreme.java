b26467d1d7f829550724ececb35c1750f62465ff
Zié Amara <90223980+ZieAmara@users.noreply.github.com>
2022-03-13 20:10:02 +0000

    Create Theoreme.java

---
+++ Theoreme.java
@@ -0,0 +1,10 @@
+package packs;
+
+import packs.Propiete;
+import packs.Axiomes;
+
+class Theoreme{
+  public static void main (String [] args){
+    System.out.println("\n\tLes operateur sont representes par les caracteres suivants : \n\t\t- ET = & \n\t\t- OU = v \n\t\t- NON = ! \n\t\t- IMPLIQUE = => \n\t\t- EQUIVAUT A = <=> \n");
+  }
+}
