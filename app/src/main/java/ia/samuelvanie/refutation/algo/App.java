58e0bef6c09b928b199bdc586c9fce61ca4d1cad
SamuelVanie <bi.vanie18@inphb.ci>
2022-04-10 02:23:51 -0400

    feat: ajouter fxml et wireframe
    - Ajout d'un fichier fxml pour l'interface
    - Ajout d'un controller FXMLController pour les evènements
    - Ajout du wireframe de l'interface en krita

--- app/src/main/java/ia/samuelvanie/refutation/algo/App.java
+++ app/src/main/java/ia/samuelvanie/refutation/algo/App.java
@@ -6,20 +6,19 @@ import static ia.samuelvanie.refutation.algo.Refutation.*;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
-        Button button = new Button();
-        root.getChildren().add(button);
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
+        Scene sc = new Scene(root);
         primaryStage.setTitle("Hello world");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(sc);
         primaryStage.show();
     }
 
---
+++ app/src/main/java/ia/samuelvanie/refutation/algo/FXMLController.java
@@ -0,0 +1,9 @@
+package ia.samuelvanie.refutation.algo;
+
+import javafx.fxml.FXML;
+
+public class FXMLController {
+    public void initialize(){
+
+    }
+}
