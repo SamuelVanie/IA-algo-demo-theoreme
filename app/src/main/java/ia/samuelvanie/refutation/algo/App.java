288b214f63188e714c2ad5ceb8eeee7bfcf613e2
SamuelVanie <bi.vanie18@inphb.ci>
2022-04-09 21:44:33 -0400

    chore: ajout de javafx
    - Ajout de la bibliotheque javafx pour l'interface
    graphique
    - Amélioration des messages de log

--- app/src/main/java/ia/samuelvanie/refutation/algo/App.java
+++ app/src/main/java/ia/samuelvanie/refutation/algo/App.java
@@ -4,9 +4,26 @@
 package ia.samuelvanie.refutation.algo;
 import static ia.samuelvanie.refutation.algo.Refutation.*;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class App {
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        Button button = new Button();
+        root.getChildren().add(button);
+        primaryStage.setTitle("Hello world");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        solve();
+        launch(args);
     }
 }
